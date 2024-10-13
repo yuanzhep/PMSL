a5578bc15536eebe66d85bfe8407b6cddd472560
yuanzhep <93398024+yuanzhep@users.noreply.github.com>
2024-10-13 17:04:31 -0400

    log

---
+++ utils/log.py
@@ -0,0 +1,26 @@
+# yz, 1012/2024
+
+import os
+import logging
+
+def get_logger(log_path, filename="log.txt"):
+    logger = logging.getLogger('logbuch')
+    logger.setLevel(level=logging.DEBUG)
+
+    # Stream
+    sh = logging.StreamHandler()
+    sh.setLevel(level=logging.DEBUG)
+    sh_formatter = logging.Formatter(
+        '[%(asctime)s][%(levelname)s] %(message)s')
+    sh.setFormatter(sh_formatter)
+
+    # File
+    fh = logging.FileHandler(os.path.join(log_path, filename))
+    fh.setLevel(level=logging.DEBUG)
+    fh_formatter = logging.Formatter(
+        '[%(asctime)s][%(levelname)s] %(message)s')
+    fh.setFormatter(fh_formatter)
+
+    logger.addHandler(sh)
+    logger.addHandler(fh)
+    return logger
